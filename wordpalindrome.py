83a5f2eb929c10f10844f4fda154be180ecfe416
Edward <76045667+edward70@users.noreply.github.com>
2021-04-19 10:21:47 +1000

    Add files via upload

---
+++ wordpalindrome.py
@@ -0,0 +1,18 @@
+def palindrome(string):
+    ''' Find palindrome of a word/setence (without punctuation) '''
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    lst = []
+    for c in string:
+        if c in chars:
+            lst.append(c)
+    length = len(lst)
+    for i in range(length//2):
+        if lst[i] != lst[length-1-i]:
+                return False
+    return True
+
+print(palindrome("hello"))
+print(palindrome("lol"))
+print(palindrome("a"))
+print(palindrome("abcd"))
+print(palindrome("...!!4929ellle"))
